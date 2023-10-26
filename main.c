b4399e1af5219ef3f3435bd78bbda30cfb174e90
Astrojarma <matsaunderz@gmail.com>
2023-10-26 01:24:29 +0000

    Created raw mode toggle functions

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios ORIGINAL_TERM_SETTINGS;
+
+int disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &ORIGINAL_TERM_SETTINGS);
+  return EXIT_SUCCESS;
+}
+
+int enableRawMode() {
+  tcgetattr(STDIN_FILENO, &ORIGINAL_TERM_SETTINGS);
+  struct termios raw = ORIGINAL_TERM_SETTINGS;
+  raw.c_lflag &= ~(ECHO | ICANON);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+  atexit((void *)disableRawMode);
+  return EXIT_SUCCESS;
+}
+
+int clearScreen() {
+  printf("%s", "\e[1;1H\e[2J");
+  return EXIT_SUCCESS;
+}
+
+int main() {
+  enableRawMode();
+  while (1) {
+  }
+  return EXIT_SUCCESS;
+}
